a0da11bf015ffd3ee07bbce45aa2e555a821171e
yg17 <yg17@illinois.edu>
2022-02-27 21:52:25 -0600

    NFA construction & acceptence check.

---
+++ src/Regex.cpp
@@ -0,0 +1,168 @@
+#include "Regex.h"
+#include <queue>
+#include <stack>
+#include <assert.h>
+
+Regex::Regex(unordered_set<char> alphabet_, string regex) {
+  alphabet = alphabet_;
+  completedExpression = "";
+  for (unsigned int i = 0; i < regex.size()-1; ++i) {
+    completedExpression += regex[i];
+    if (isSymbol(regex[i]) && isSymbol(regex[i+1])) 
+      completedExpression += '.';
+    if ((regex[i] == '*' || regex[i] == ')') && isSymbol(regex[i+1]))
+      completedExpression += '.';
+  }
+  completedExpression += regex[regex.size()-1];
+  reversePolishNotation = convertToRPN(completedExpression);
+  generateNFA(reversePolishNotation);
+}
+
+/*
+Convert the regular expression to the reverse polish notation.
+*/
+string Regex::convertToRPN(string comletedExpression) {
+  queue<char> output;
+  stack<char> operators;
+  for (unsigned int i = 0; i < completedExpression.size(); ++i) {
+    char a = completedExpression[i];
+    if (isSymbol(a)) {
+      output.push(a);
+    } else if (a == '*') {
+      output.push(a);
+    } else if (a == '.') {
+      while (!operators.empty() && operators.top() == '.') {
+        char concate = operators.top();
+        operators.pop();
+        output.push(concate);
+      }
+      operators.push(a);
+    } else if (a == '+') {
+      while (!operators.empty() && operators.top() == '.') {
+        char concate = operators.top();
+        operators.pop();
+        output.push(concate);
+      }
+      operators.push(a);
+    } else if (a == '(') {
+      operators.push(a);
+    } else if (a == ')') {
+      assert(!operators.empty() && "Mismatched parentheses.");
+      while (operators.top() != '(') {
+        char op = operators.top();
+        operators.pop();
+        output.push(op);
+        assert(!operators.empty() && "Mismatched parentheses.");
+      }
+      operators.pop();
+    }
+  }
+  while (!operators.empty()) {
+    char op = operators.top();
+    operators.pop();
+    output.push(op);
+  }
+  string RPN = "";
+  while (!output.empty()) {
+    RPN += output.front();
+    output.pop();
+  }
+  return RPN;
+}
+
+/*
+Generate the corresponding NFA using Thompson's Algorithm.
+*/
+void Regex::generateNFA(string RPN) {
+  stack<NFA> chain;
+  for (unsigned int i = 0; i < RPN.size(); ++i) {
+    char a = RPN[i];
+    if (isSymbol(a)) {
+
+      NFA::NFAState* s_ = new NFA::NFAState();
+      NFA::NFAState* e_ = new NFA::NFAState();
+      s_->outs[a] = unordered_set<NFA::NFAState*>{e_};
+      NFA singleton(s_, e_);
+      chain.push(singleton);
+
+    } else if (a == '.') {
+
+      NFA first = chain.top();
+      chain.pop();
+      NFA second = chain.top();
+      chain.pop();
+      first.e->outs['$'] = unordered_set<NFA::NFAState*>{second.s};
+      NFA combination(first.s, second.e);
+      chain.push(combination);
+
+    } else if (a == '+') {
+
+      NFA first = chain.top();
+      chain.pop();
+      NFA second = chain.top();
+      chain.pop();
+      NFA::NFAState* s_ = new NFA::NFAState();
+      NFA::NFAState* e_ = new NFA::NFAState();
+      s_->outs['$'] = unordered_set<NFA::NFAState*>{first.s, second.s};
+      first.e->outs['$'] = unordered_set<NFA::NFAState*>{e_};
+      second.e->outs['$'] = unordered_set<NFA::NFAState*>{e_};
+      NFA combination(s_, e_);
+      chain.push(combination);
+
+    } else if (a == '*') {
+
+      NFA first = chain.top();
+      chain.pop();
+      NFA::NFAState* s_ = new NFA::NFAState();
+      NFA::NFAState* e_ = new NFA::NFAState();
+      s_->outs['$'] = unordered_set<NFA::NFAState*>{first.s, e_};
+      e_->outs['$'] = unordered_set<NFA::NFAState*>{s_};
+      first.e->outs['$'] = unordered_set<NFA::NFAState*>{e_};
+      NFA combination(s_, e_);
+      chain.push(combination);
+      
+    }
+  }
+  nfa = chain.top();
+}
+
+bool Regex::isSymbol(char token) {
+  return token != '(' && token != ')' && token != '*' && token != '+' && token != '.';
+}
+
+bool Regex::check(string str) {
+  unsigned int i = 0;
+  queue<NFA::NFAState*> buffer;
+  buffer.push(nfa.s);
+  NFA::NFAState* sentinal = new NFA::NFAState();
+  buffer.push(sentinal);
+  while (buffer.front() != sentinal) {
+    NFA::NFAState* q = buffer.front();
+    buffer.pop();
+    if (i == str.size()) {
+      if (checkEpsilonReach(q)) return true;
+    }
+    for (NFA::NFAState* out : q->outs[str[i]]) {
+      buffer.push(out);
+    }
+    for (NFA::NFAState* epsilonReach : q->outs['$']) {
+      for (NFA::NFAState* out : epsilonReach->outs[str[i]]) {
+        buffer.push(out);
+      }
+    }
+    if (buffer.front() == sentinal) {
+      buffer.pop();
+      buffer.push(sentinal);
+      i++;
+    }
+  }
+  return false;
+}
+
+bool Regex::checkEpsilonReach(NFA::NFAState* q) {
+  if (q == nfa.e) return true;
+  for (NFA::NFAState* out : q->outs['$']) {
+    if (checkEpsilonReach(out)) return true;
+  }
+  return false;
+}
--- src/Regex.h
+++ src/Regex.h
@@ -1,37 +1,65 @@
-#include<string>
-#include<vector>
-#include<unordered_map>
-#include<unordered_set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class Regex {
   private:
     unordered_set<char> alphabet;
-    string expression;
+    string completedExpression;
+    string reversePolishNotation;
+
+    string convertToRPN(string completedExpression);
+    bool isSymbol(char token);
 
   private:
     class NFA {
-      private:
+      public:
         class NFAState {
-          private:
+          public:
             unordered_map<char, unordered_set<NFAState*>> outs;
         };
       
-      private:
+      public:
         unordered_set<char> alphabet_NFA;
         unordered_set<NFAState*> Q;
         NFAState* s;
+        NFAState* e;
         unordered_set<NFAState*> A;
         unordered_set<NFAState*> delta(NFAState* q, char a);
       
       public:
-        NFA(Regex regex);
-        ~NFA();
+        NFA() {/* Nothing */}
+        NFA(NFAState* s_, NFAState* e_) : s(s_), e(e_) {}
+        void clear();
     };
   
+    class DFA {
+      private:
+        class DFAState {
+          private:
+            unordered_map<char, DFAState*> outs;
+        };
+      
+      private:
+        unordered_set<char> alphabet_DFA;
+        unordered_set<DFAState*> Q;
+        DFAState* s;
+        unordered_set<DFAState*> A;
+        DFAState* delta(DFAState* q, char a);
+    };
+
+  private:
+    NFA nfa;
+    DFA dfa;
+
+    void generateNFA(string RPN);
+  
   public:
-    Regex(unordered_set<char> alphabet, string expression);
+    Regex(unordered_set<char> alphabet_, string regex);
     bool operator==(const Regex &rhs) const;
     bool check(string str);
+    bool checkEpsilonReach(NFA::NFAState* q);
 };
